cc6242685d4db109a1808ab7603cdac9ca3e632f
Padma Keerthana D <keerthanadhanasekaran62@gmail.com>
2025-10-04 23:01:23 +0530

    commit 1

---
+++ src/com/Tnsif/dayfour/hierarchy/Employee.java
@@ -0,0 +1,37 @@
+package com.Tnsif.dayfour.hierarchy;
+
+public class Employee extends Person{
+	private int empId;
+	private float salary;
+	private String dept;
+	
+	
+	public Employee() {
+		System.out.println("Employee Class Default Constructor");
+	}
+
+
+	public Employee(int empId, float salary, String dept) {
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+	
+	
+	public Employee(String name, String city,int empId, float salary, String dept) {
+		super(name,city);
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+	}
+
+
+	public int getEmpId() {
+		return empId;
+	}
+
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+}
