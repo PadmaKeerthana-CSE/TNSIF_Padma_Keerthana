6d82f75a50b819d2f89582c1c520b13e74e3b361
Padma Keerthana D <keerthanadhanasekaran62@gmail.com>
2025-10-29 12:32:30 +0530

    commit jdbc

---
+++ src/com/Tnsif/jdbcdemo/PreparedStatementDemo.java
@@ -0,0 +1,69 @@
+package com.Tnsif.jdbcdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PreparedStatementDemo {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:postgresql://localhost:5432/student_portal";
+		String username = "postgres";
+		String password = "keerthana143";
+		
+		String studentName = "Shruthi";
+		String studentDept = "IT";
+		int yearOfPassing = 2024;
+		
+		
+		String query = "INSERT INTO student_details(student_name, student_dept, year_of_passing) VALUES(?, ?, ?) ";
+		
+		
+		try {
+			
+			//Load the driver - 2nd step
+			Class.forName("org.postgresql.Driver");
+			
+			
+			//Establishing the connection - 3rd step
+			
+			Connection con = DriverManager.getConnection(url, username, password);
+			
+			//Define SQL Query - 4th step
+			
+			PreparedStatement st = con.prepareStatement(query);
+			
+			//Execting the query - 5th step
+			
+			st.setString(1, studentName);
+			st.setString(2, studentDept);
+			st.setInt(3, yearOfPassing);
+			
+			
+			//Process the Result - 6th Step
+			
+			int rs = st.executeUpdate();
+			System.out.println(rs +" row/s affected");
+			
+			
+			
+			//close the resources - 7th
+			
+			st.close();
+			con.close();
+			
+			
+		}
+		
+
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+
+
+	}
+
+}
